06da613de440a389a83cc6bf45eec61e6bc5ebba
courtneyjcunningham <103600302+courtneyjcunningham@users.noreply.github.com>
2022-04-26 16:03:49 -0700

    Added/ Pokemon Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('hello world');
-
-let favoriteFood = 'Depends on the day';
-document.write(favoriteFood);
+let pokemonList = [{
+  name: 'Butterfree',
+  height: 1.1m,
+  type: 'Bug',
+},
+{
+  name: 'Jigglypuff',
+  height: 0.5m,
+  type: 'Fairy',
+},
+{
+  name: 'Pikachu',
+  height: 0.4m,
+  type: 'Electric',
+}
+];
